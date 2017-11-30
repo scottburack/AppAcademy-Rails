4634c78132c57c05c8c2f5af8aad9d2b9268ad00
scottburack <32395163+scottburack@users.noreply.github.com>
2017-11-30 08:09:05 -0500

    Add files via upload

---
+++ 99Cats/app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'everybody@appacademy.io'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/session/new'
+    mail(to: user.username, subject: 'Welcome to 99 cats!')
+  end
+  
+end
